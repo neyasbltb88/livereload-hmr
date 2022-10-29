633a5b92ea9364d251ec01a6789e2ace4f67ec61
neyasbltb88 <neyasbltb_88@mail.ru>
2022-10-30 01:54:18 +0300

    В утилите dom добавлена возможность в jsx классы передавать не только в атрибуте className, но и в более привычном простом class

--- src/scripts/utils/dom.ts
+++ src/scripts/utils/dom.ts
@@ -1,27 +1,41 @@
+import reporter from './reporter';
+
 const dom = (tag: string | Function, attrs: Record<string, any>, ...children: (HTMLElement | string | number)[]) => {
-    // Custom Components will be functions
-    if (typeof tag === 'function') {
-        return tag();
-    }
-    // regular html tags will be strings to create the elements
+    // Другие JSX компоненты будут функциями dom, их надо просто вызвать
+    if (typeof tag === 'function') return tag();
+
+    // Обычные HTML-теги будут приходить сюда строками
     if (typeof tag === 'string') {
-        // fragments to append multiple children to the initial node
-        const fragments = document.createDocumentFragment();
         const element = document.createElement(tag);
+
+        // TODO: Реализовать возможность передавать в class или className объект и массив
+        if (attrs?.class) {
+            const className = attrs.className ? attrs.className + ' ' : '';
+            attrs.className = className + attrs.class;
+            delete attrs.class;
+        }
+        // Вкидываем на элемент объект атрибутов
+        Object.assign(element, attrs);
+
+        // Фрагменты для добавления нескольких дочерних элементов к начальному узлу
+        const fragments = document.createDocumentFragment();
+
+        // Обрабатываем дочерние элементы
         children.forEach(child => {
             if (child instanceof HTMLElement) {
+                // HTML-элементы сразу добавляем к фрагментам как есть
                 fragments.appendChild(child);
             } else if (typeof child === 'string' || typeof child === 'number') {
+                // Из строк и чисел сначала делаем текстовые ноды перед добавлением к фрагментам
                 const textnode = document.createTextNode(String(child));
                 fragments.appendChild(textnode);
             } else {
-                // later other things could not be HTMLElement not strings
-                // console.log('not appendable', child);
+                // На случай, если в качестве дочерних элементов попадет что-то странное
+                reporter.log('Дочерний элемент не поддерживаемого типа', { tag, attrs, child });
             }
         });
         element.appendChild(fragments);
-        // Merge element with attributes
-        Object.assign(element, attrs);
+
         return element;
     }
 };
