adb34bcbdbbe5238ea182cfa9b4e30b2a5fe792c
neyasbltb88 <neyasbltb_88@mail.ru>
2022-10-30 00:39:24 +0300

    Проработана возможность создавать .tsx компоненты. Main.js переименован в Main.jsx + из него убран явный импорт dom

--- src/scripts/template/Main.jsx
+++ src/scripts/template/Main.jsx
@@ -1,10 +1,13 @@
-import { dom } from '@utils';
 import './styles/style.css';
+
+import TSXComponent from './TSXComponent';
 
 export default function Main() {
     return (
         <div className="header-container">
             <h1>Hello from Livereload HMR!</h1>
+            <div className="plus">+</div>
+            <TSXComponent />
         </div>
     );
 }
---
+++ src/scripts/template/TSXComponent.tsx
@@ -0,0 +1,7 @@
+import { random } from "@utils";
+
+export default () => {
+    return (
+        <div className="TSXComponent">TSX_Component {random(0, 100)}</div>
+    )
+}
